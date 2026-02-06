d2ab06c42ab9b7accd4312d9ad17818ebf5558b2
Guillermo Hernandez <guillermohernandez@gmail.com>
2026-02-06 17:21:07 -0600

    added styling daisyui

--- src/App.jsx
+++ src/App.jsx
@@ -46,65 +46,63 @@ function App() {
       {/* Input Fields */}
       <h1 className="text-green-600 text-5xl font-bold">Clippy</h1>
       <div className="row-span-full">
-        <div>
+        <fieldset className="fieldset">
+          <legend className="fieldset-legend">Enter</legend>
           <label htmlFor="cycleSet">Cycle Set:</label>
 
           <input
+            className="input join-item"
             id="cycleSet"
             type="text"
             value={inputs.cycleSet}
             onChange={(e) => handleChange("cycleSet", e.target.value)}
             aria-label="Cycle Set"
           />
-        </div>
 
-        <div>
           <label htmlFor="cycle">Cycle:</label>
 
           <input
+            className="input join-item"
             id="cycle"
             type="text"
             value={inputs.cycle}
             onChange={(e) => handleChange("cycle", e.target.value)}
             aria-label="Cycle"
           />
-        </div>
 
-        <div>
           <label htmlFor="teamNumber">Team Number:</label>
 
           <input
+            className="input join-item"
             id="teamNumber"
             type="text"
             value={inputs.teamNumber}
             onChange={(e) => handleChange("teamNumber", e.target.value)}
             aria-label="Team Number"
           />
-        </div>
 
-        <div>
           <label htmlFor="srNumber">SR Number:</label>
 
           <input
+            className="input join-item"
             id="srNumber"
             type="text"
             value={inputs.srNumber}
             onChange={(e) => handleChange("srNumber", e.target.value)}
             aria-label="SR Number"
           />
-        </div>
 
-        <div>
           <label htmlFor="rits">SR Number:</label>
 
           <input
+            className="input join-item"
             id="rits"
             type="text"
             value={inputs.rits}
             onChange={(e) => handleChange("rits", e.target.value)}
             aria-label="RITS"
           />
-        </div>
+        </fieldset>
       </div>
 
       {/* Display Values */}
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer class="text-base-content ">
+      <aside class="bg-base-300 py-4 px-8 w-full flex gap-2 flex-wrap justify-between items-center text-sm">
+        <p>Copyright © 2026</p>
+      </aside>
+    </footer>
+  );
+}
